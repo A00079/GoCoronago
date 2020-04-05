d0c9b234247fd64c50e6545fd6e2a02475a6a8d0
A0079 <bajay00007@gmail.com>
2020-04-05 12:44:38 -0700

    App Fixed For Production

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.use(cookieParser());
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-// }
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
